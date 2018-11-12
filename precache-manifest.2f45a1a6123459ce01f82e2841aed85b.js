fad1936f0861a5e1e76c027b2355fa457898c38c
Stanislaw <gregor.stanislaw@hotmail.com>
2018-11-12 01:01:14 +0100

    Updates

---
+++ precache-manifest.2f45a1a6123459ce01f82e2841aed85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99843d54bd71a8bc8dda",
+    "url": "/note-taking-app/static/js/runtime~main.99843d54.js"
+  },
+  {
+    "revision": "f2b31bd507f2c4ea2c86",
+    "url": "/note-taking-app/static/js/main.f2b31bd5.chunk.js"
+  },
+  {
+    "revision": "28b7948ef1a98ed3e0c5",
+    "url": "/note-taking-app/static/js/1.28b7948e.chunk.js"
+  },
+  {
+    "revision": "f2b31bd507f2c4ea2c86",
+    "url": "/note-taking-app/static/css/main.ff71d835.chunk.css"
+  },
+  {
+    "revision": "28b7948ef1a98ed3e0c5",
+    "url": "/note-taking-app/static/css/1.a5b78b0e.chunk.css"
+  },
+  {
+    "revision": "09e436a804b87bd745d1b852ada3475b",
+    "url": "/note-taking-app/index.html"
+  }
+];
